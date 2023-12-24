95e528442b23e2d453b293e670c864d0c0ec6f4e
Thomas <thomaslblc@gmail.com>
2023-12-24 15:47:28 +0100

    STYLE homepage + ADD favicon + ADD pages + SCRIPT filter

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     var matchFilters = document.querySelectorAll('.matchs-filter');
+    var matchMainDivs = document.querySelectorAll('.match-main');
 
     // Ajoute un gestionnaire d'événement à chaque élément li
-    matchFilters.forEach(function (filter) {
+    matchFilters.forEach(function (filter, index) {
         filter.addEventListener('click', function () {
             // Retire la classe .filter-active de l'élément actuellement actif
             var activeFilter = document.querySelector('.filter-active');
@@ -14,15 +15,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Ajoute la classe .filter-active à l'élément cliqué
             filter.classList.add('filter-active');
+
+            // Masque tous les contenus alternatifs
+            matchMainDivs.forEach(function (div) {
+                div.classList.remove('filter-active-content');
+            });
+
+            // Affiche le contenu alternatif correspondant
+            var selectedContentClass = 'equipe' + filter.classList[1].substring(6);
+            var selectedContentDiv = document.querySelector('.' + selectedContentClass);
+            if (selectedContentDiv) {
+                selectedContentDiv.classList.add('filter-active-content');
+            }
         });
     });
 });
-
-// SLIDER BANNER //
-
-document.addEventListener('DOMContentLoaded', function() {
-    const bannerText = document.getElementById('bannerText');
-    const originalText = bannerText.textContent;
-    const repeatedText = `${originalText} ${originalText} ${originalText}`;
-    bannerText.textContent = repeatedText;
-  });
